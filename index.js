1872a5587076bf6176553f9a6c57275b48bfb581
hortonr6 <hortonr6@coventry.ac.uk>
2019-10-18 00:18:11 +0100

    Switched to command variable and added extra commands

--- index.js
+++ index.js
@@ -24,8 +24,44 @@ client.on('message', message => {
 	// If a message is not a command and or it is from a bot, then ignore it
 	if(!message.content.startsWith(prefix) || message.author.bot) return;
     
-	// Ping Pong
-	if (message.content.startsWith(prefix + 'ping')) {
+	// Gets everything after the command prefix and splits it whenever there's whitespace (uses Regex)
+	const args = message.content.slice(prefix.length).split(/ +/);
+	// shift() removes the first element of the array and returns it, here it removes the command name and stores it in the command constant
+	const command = args.shift().toLowerCase();
+
+	if (command === 'kick') {
+		// Checks if a user was mentioned before executing the command
+		if(!message.mentions.users.size) {
+			// message.reply() automatically tags the message sender in the response
+			return message.reply('You have to mention someone to ask to kick them');
+		}
+		// Gets the first user tagged in the arguments
+		const taggedUser = message.mentions.users.first();
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	} else if (command === 'ping') {
+		// Responds to ping with pong
 		message.channel.send('pong <@' + message.author.id + '>');
+	} else if (command === 'beep') {
+		// Responds to beep with boop
+		message.reply('boop');
+	} else if (command === 'server-info') {
+		// Servers are called guilds in the discord.js API
+		message.channel.send(`This server is called: ${message.guild.name}\nThe number of users in this server is: ${message.guild.memberCount}`);
+	} else if (command === 'user-info') {
+		// Gives the user their information
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	} else if (command === 'avatar') {
+		if(!message.mentions.users.size) {
+			// Gives command sender's avatar url
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL}>`);
+		} else {
+			// eslint-disable-next-line no-unused-vars
+			const avatarList = message.mentions.users.map(user => {
+				// Loops through all mentioned users and gives avatar URL
+				return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
+			});
+
+			message.channel.send(avatarList);
+		}
 	}
 });
